2e1611fdb3318bd0b000228f4e08183775fcacca
J0nnyGM <johny5230@gmail.com>
2026-09-18 15:21:02 -0500

    Pixeltech V10.17

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,7 +24,7 @@ self.addEventListener('install', (event) => {
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', (event) => { 
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
